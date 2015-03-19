6a5fe1c8fa7fb8591bc5726d2c1518bbe9a360cb
Carra Wu <carrasw@gmail.com>
2015-03-19 13:30:42 -0400

    Initial Commit

---
+++ Sous/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sous
+//
+//  Created by Summer Wu on 3/19/15.
+//  Copyright (c) 2015 buz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
